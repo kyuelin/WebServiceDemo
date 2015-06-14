2ee7df9466fc9a45b70e54653626b8647c6285c8
kyuelin <kyuelin@gmail.com>
2015-06-14 02:55:00 -0400

    new codes

--- StockQuote/src/dev/kyuelin/extservice/stocks/demo/NetClientGet.java
+++ StockQuote/src/dev/kyuelin/extservice/stocks/demo/NetClientGet.java
@@ -1,7 +1,41 @@
 package dev.kyuelin.extservice.stocks.demo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 /**
  * Created by linken on 6/14/15.
  */
 public class NetClientGet {
+    public static void main(String[] args) {
+        try {
+            URL url = new URL("http://www.xignite.com/xRealTime.json/GetRealQuote?Exchange=INET&Symbol=ms&IncludeBidAsk=false");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept","application/json");
+            if (conn.getResponseCode()!=200) {
+                throw new RuntimeException("Failed: HTTP error code: " + conn.getResponseCode());
+            }
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            String output;
+            System.out.println("Ouput from server ...");
+            while((output = br.readLine())!=null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
